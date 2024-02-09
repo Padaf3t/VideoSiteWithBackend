e9647cb5b19355aef95040cb098cd403e74fcac4
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-09 14:26:32 -0500

    Commit Squelette Catalogue

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Catalogue.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Catalogue.cs
@@ -1,12 +1,47 @@
-﻿using Catalogue;
+﻿
 
-namespace Application
+namespace Catalogue
 {
-    internal class Program
+    internal class Catalogue
     {
         private List<Video> _listeVideo;
         private List<Utilisateur> _listeUtilisateur;
 
+        public Catalogue()
+        {
+            _listeVideo = new List<Video>();
+            _listeUtilisateur = new List<Utilisateur>();
+        }
+
+        private bool AjouterVideo(Video video)
+        {
+            return true;
+        }
+
+        private bool AjouterVideo(Video[] videos)
+        {
+            return true;
+        }
+
+        private bool RemplacerVideo(Video videoARetirer, Video videoAAjouter)
+        {
+            return true;
+        }
+
+        private bool SupprimerVideo(Video video)
+        {
+            return true;
+        }
+
+        private bool SauvegarderVideo()
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
 
         static void Main(string[] args)
         {
