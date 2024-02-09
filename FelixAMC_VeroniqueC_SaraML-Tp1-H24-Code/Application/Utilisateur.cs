75b0c3e43133d4b56322a706b78bec976e42a2f6
veroniquecloutier <2265278@cegeplimoilou.ca>
2024-02-09 13:54:42 -0500

    Update Utilisateur.cs
    pré commit

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Utilisateur.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Utilisateur.cs
@@ -8,5 +8,16 @@ namespace Catalogue
 {
     internal class Utilisateur
     {
+        private string _pseudo;
+        private string _motDePasse;
+        private string _nom;
+        private string _prenom;
+        private enum Role
+        { 
+            UtilisateurSimple,
+            Admin,
+            Technicien
+        }
+
     }
 }
