9b3d82f995a91ee82d3251a5229d8a4cb9b7c7bd
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-09 14:31:34 -0500

    Commit Constructeur Evaluation

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Evaluation.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Evaluation.cs
@@ -13,7 +13,17 @@ namespace Catalogue
         {
             Mediocre = 0, Mauvais = 1, Moyen = 2, Bon = 3, Excellent = 4, Extraordinaire = 5
         }
+
+        public Cote _cote;
         public int _idVideo;
         public int _pseudoUtilisateur;
+
+        public Evaluation(int pidVideo, int ppseudoUtilisateur, Cote pcote)
+        {
+            _idVideo = pidVideo;
+            _pseudoUtilisateur = ppseudoUtilisateur;
+            _cote = pcote;
+            
+        }
     }
 }
