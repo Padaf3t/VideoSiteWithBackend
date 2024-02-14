829a8fa752f4cce1405f793029a898eaaab1c105
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-14 10:17:50 -0500

    Creation Application et ajout liste User

---
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Application.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalogue
+{
+    public class Application
+    {
+        private List<Utilisateur> _listeUtilisateur;
+
+        internal List<Utilisateur> ListeUtilisateur { get => _listeUtilisateur; set => _listeUtilisateur = value; }
+
+        internal bool AjouterUtilisateur(Utilisateur user)
+        {
+            int longeurInitial = ListeUtilisateur.Count;
+            ListeUtilisateur.Add(user);
+            return ListeUtilisateur.Count > longeurInitial;
+        }
+
+        private List<Utilisateur> AjouterJSONUtilisateur(string fichier)
+        {
+            return this.ListeUtilisateur;
+        }
+
+        private void SauvegarderUtilisateurs(string fichier)
+        {
+
+        }
+    }
+}
