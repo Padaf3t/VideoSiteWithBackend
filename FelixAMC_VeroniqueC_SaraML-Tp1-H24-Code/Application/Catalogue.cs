60aa83d947cc6f700dcce91ff1befb93a6f9e774
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-14 10:16:16 -0500

    Retrait listeUtilisateur pour mettre dans Application

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Catalogue.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Catalogue.cs
@@ -5,27 +5,13 @@ namespace Catalogue
     internal class Catalogue
     {
         private List<Video> _listeVideo;
-        private List<Utilisateur> _listeUtilisateur;
 
         internal List<Video> ListeVideo { get => _listeVideo; set => _listeVideo = value; }
-        internal List<Utilisateur> ListeUtilisateur { get => _listeUtilisateur; set => _listeUtilisateur = value; }
+        
 
         public Catalogue()
         {
             ListeVideo = new List<Video>();
-            ListeUtilisateur = new List<Utilisateur>();
-        }
-
-        public bool AjouterUtilisateur(Utilisateur user)
-        {
-            int longeurInitial = ListeUtilisateur.Count;
-            ListeUtilisateur.Add(user);
-            return ListeUtilisateur.Count > longeurInitial;
-        }
-
-        private List<Utilisateur> AjouterJSONUtilisateur(string fichier)
-        {
-            return this.ListeUtilisateur;
         }
 
         public bool AjouterVideo(Video video)
@@ -36,24 +22,17 @@ namespace Catalogue
 
         }
 
-        private List<Video> AjouterJSONVideo(string fichier)
+        public List<Video> AjouterJSONVideo(string fichier)
         {
             return this.ListeVideo;
         }
 
-        public bool AjouterJSON(string fichierVideo, string fichierUtilisateur)
-        {
-            AjouterJSONUtilisateur(fichierUtilisateur);
-            AjouterJSONVideo(fichierVideo);
-            return true;
-        }
-
-        private bool RemplacerVideo(Video videoARetirer, Video videoAAjouter)
+        public bool RemplacerVideo(Video videoARetirer, Video videoAAjouter)
         {
             return true;
         }
 
-        private bool SupprimerVideo(Video video)
+        public bool SupprimerVideo(Video video)
         {
             return true;
         }
@@ -66,17 +45,6 @@ namespace Catalogue
         private void SauvegarderVideos(string fichier)
         {
             
-        }
-
-        private void SauvegarderUtilisateurs(string fichier)
-        {
-
-        }
-
-        public void SauvegarderLeCatalogue(string fichierUtilisateur, string fichierVideo)
-        {
-            SauvegarderUtilisateurs(fichierUtilisateur);
-            SauvegarderVideos(fichierVideo);
         }
 
         public override string ToString()
