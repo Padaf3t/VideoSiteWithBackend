af7a17a8fc1346565d5ff50b91a5cae1e659404a
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-15 08:10:20 -0500

    Renommer namespace Catalogue par ProjetCatalogue

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Evaluation.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Evaluation.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Catalogue
+namespace ProjetCatalogue
 {
     internal struct Evaluation : IEquatable<Evaluation>
     {
