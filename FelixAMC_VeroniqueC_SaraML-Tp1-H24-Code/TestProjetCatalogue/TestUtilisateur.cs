e2acc807871b847fac2d0acc37cd15ad26f0bdde
Sara-Maud <158103864+Sara-Maud@users.noreply.github.com>
2024-02-16 13:00:39 -0500

    Update TestUtilisateur.cs
    Ajout test constructeur pour utilisateur

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestUtilisateur.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestUtilisateur.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetCatalogue;
 
 namespace TestProjetCatalogue
 {
     internal class TestUtilisateur
     {
+        [Test]
+        public void etantConstrucUilisateur_quandUtilisateurAvecBonneDonne_alorsUtilisateurAjouter()
+        {
+            string pseudo = "First!";
+            string motDePasse = "abc123!";
+            Utilisateur utilisateur = new Utilisateur(pseudo, motDePasse);
+
+        }
+
+        [Test]
+        public void etantConstrucUilisateur_quandUtilisateurAvecMauvaiseDonne_alorsUtilisateurPasAjouter()
+        {
+            string pseudo = "Bla";
+            string motDePasse = "";
+            Utilisateur utilisateur = new Utilisateur(pseudo, motDePasse);
+        
+        }
+
     }
 }
