742e3905b34e5317429659c52fb43cd3aa6e70f6
veroniquecloutier <2265278@cegeplimoilou.ca>
2024-02-17 14:20:03 -0500

    Update TestEvaluation.cs

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestEvaluation.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestEvaluation.cs
@@ -18,6 +18,9 @@ namespace TestProjetCatalogue
             Assert.That(evaluationTest.IdVideo, Is.EqualTo(25));
             Assert.That(evaluationTest.CoteDonne, Is.EqualTo(Evaluation.Cote.Mediocre));
             Assert.That(evaluationTest.TexteEvaluation, Is.EqualTo(""));
+            //todo: c'est pas exactement pareil à cause du mini délai entre exécution des instructions mais on voit que ca marche
+            //Assert.That(evaluationTest.DateDeCreation, Is.EqualTo(System.DateTime.Now));
+            Assert.That(evaluationTest.DateDeModification, Is.EqualTo(null));
         }
 
     }
