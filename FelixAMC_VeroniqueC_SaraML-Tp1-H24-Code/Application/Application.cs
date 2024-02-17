6a065c25c18d90ca37e2089e2751ae6c9754c3ae
veroniquecloutier <2265278@cegeplimoilou.ca>
2024-02-17 14:42:28 -0500

    Doc classe Application

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Application.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Application.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 
 namespace ProjetCatalogue
 {
+
+    /// <summary>
+    /// Classe qui constitue l'application, c'est la classe principale qui contient le Main donc l'entrée dans le programme
+    /// </summary>
     public class Application
     {
+
         private List<Utilisateur> _listeUtilisateur;
         private Catalogue _catalogueApplication;
 
+        /// <summary>
+        /// Constructeur de la classe, sans paramètre. Crée une liste d'utilisateurs et un catalogue pour l'application
+        /// </summary>
         public Application()
         {
             ListeUtilisateur = new List<Utilisateur>();
@@ -21,12 +29,23 @@ namespace ProjetCatalogue
         internal List<Utilisateur> ListeUtilisateur { get => _listeUtilisateur; set => _listeUtilisateur = value; }
         internal Catalogue CatalogueApplication { get => _catalogueApplication; set => _catalogueApplication = value; }
 
+        /// <summary>
+        /// Permet l'ajout d'un utilisateur à la liste d'utilisateurs de l'application
+        /// </summary>
+        /// <param name="user">L'utilisateur à ajouter</param>
+        /// <returns>bool : true si l'ajout a bien été effectué</returns>
         internal bool AjouterUtilisateur(Utilisateur user)
         {
             ListeUtilisateur.Add(user);
             return ListeUtilisateur.Last() == user;
         }
 
+        /// <summary>
+        /// Méthode qui permet la désérialisation d'un fichier JSON pour en extraire des objets Java Utilisateurs
+        /// et les placer dans une liste d'utilisateurs
+        /// </summary>
+        /// <param name="fichier">Le fichier JSON utilisé</param>
+        /// <returns>List(Utilisateur): la liste d'utilisateurs ainsi créée</returns>
         private List<Utilisateur> AjouterJSONUtilisateur(string fichier)
         {
             //TODO : gerer erreurs
@@ -38,6 +57,10 @@ namespace ProjetCatalogue
             return listeUtilisateurs;
         }
 
+        /// <summary>
+        /// Permet de prendre une liste d'utilisateurs et de la sérialiser dans un fichier JSON
+        /// </summary>
+        /// <param name="fichier">Le fichier JSON à utiliser</param>
         private void SauvegarderUtilisateurs(string fichier)
         {
             string jsonListe = JsonConvert.SerializeObject(this.ListeUtilisateur, this.ListeUtilisateur.GetType(), Formatting.Indented, new JsonSerializerSettings
@@ -48,6 +71,10 @@ namespace ProjetCatalogue
             File.WriteAllText(@fichier, jsonListe);
         }
 
+        /// <summary>
+        /// Méthode d'entrée dans le programme
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             
