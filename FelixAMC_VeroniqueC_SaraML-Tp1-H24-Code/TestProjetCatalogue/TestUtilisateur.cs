a8a23c3aaed25fa04b14c61e8b9b9b8ede9b66ef
Sara-Maud <158103864+Sara-Maud@users.noreply.github.com>
2024-02-21 10:48:01 -0500

    commit testUtilisateur

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestUtilisateur.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestUtilisateur.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ProjetCatalogue;
 
@@ -16,6 +17,23 @@ namespace TestProjetCatalogue
         {
             
             Utilisateur utilisateur = new Utilisateur(pseudoBon, motDePasseBon);
+            private string VerifierPseudo(string pseudo)
+            {
+
+                if (pseudo is null || pseudo.Length < 5 || pseudo.Length > 20 || Regex.Matches(pseudo, "^\\w+$").Count == 0)
+                {
+                    throw new ArgumentException("Le pseudo " + pseudo + " n'est pas valide");
+                }
+                return pseudo;
+            }
+            private string VerifierMotDePasse(string motDePasse)
+            {
+                if (motDePasse.Length < 8 || Regex.Matches(motDePasse, "\\w+$").Count == 0)
+                {
+                    throw new ArgumentException("Le mot de passe n'est pas valide");
+                }
+                return motDePasse;
+            }
 
         }
 
