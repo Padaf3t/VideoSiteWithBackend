b2c3be18327d582aadc55ad220d202ed12ebee67
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-21 10:02:32 -0500

    Déplacement methode gestion listeUtilisateur à GestionUtilisateur

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Application.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/Application.cs
@@ -16,7 +16,7 @@ namespace ProjetCatalogue
     public class Application
     {
 
-        private List<Utilisateur> _listeUtilisateur;
+        private GestionUtilisateur _listeUtilisateur;
         private Catalogue _catalogueApplication;
 
         /// <summary>
@@ -24,62 +24,14 @@ namespace ProjetCatalogue
         /// </summary>
         public Application()
         {
-            ListeUtilisateur = new List<Utilisateur>();
+            ListeUtilisateur = new GestionUtilisateur();
             CatalogueApplication = new Catalogue();
         }
 
-        internal List<Utilisateur> ListeUtilisateur { get => _listeUtilisateur; set => _listeUtilisateur = value; }
+        internal GestionUtilisateur ListeUtilisateur { get => _listeUtilisateur; set => _listeUtilisateur = value; }
         internal Catalogue CatalogueApplication { get => _catalogueApplication; set => _catalogueApplication = value; }
 
-        /// <summary>
-        /// Permet l'ajout d'un utilisateur à la liste d'utilisateurs de l'application
-        /// </summary>
-        /// <param name="user">L'utilisateur à ajouter</param>
-        /// <returns>bool : true si l'ajout a bien été effectué</returns>
-        internal bool AjouterUtilisateur(Utilisateur user)
-        {
-            ListeUtilisateur.Add(user);
-            return ListeUtilisateur.Last() == user;
-        }
-
-        /// <summary>
-        /// Méthode qui permet la désérialisation d'un fichier JSON pour en extraire des objets C# Utilisateurs
-        /// et les placer dans une liste d'utilisateurs
-        /// </summary>
-        /// <param name="fichier">Le fichier JSON utilisé</param>
-        /// <returns>List(Utilisateur): la liste d'utilisateurs ainsi créée</returns>
-        private List<Utilisateur> AjouterJSONUtilisateur(string fichier)
-        {
-            List<Utilisateur>? listeUtilisateurs = null;
-            try
-            {
-                 listeUtilisateurs = JsonConvert.DeserializeObject<List<Utilisateur>>(File.ReadAllText(@fichier), new JsonSerializerSettings
-                 {
-                    TypeNameHandling = TypeNameHandling.Auto
-                 });
-            }
-            catch (FileNotFoundException e){
-                Console.WriteLine("Le fichier {0} n'a pas été trouvé", fichier);
-            }
-
-            return listeUtilisateurs;
-            
-        }
-
-        /// <summary>
-        /// Permet de prendre une liste d'utilisateurs et de la sérialiser dans un fichier JSON
-        /// </summary>
-        /// <param name="fichier">Le fichier JSON à utiliser</param>
-        private void SauvegarderUtilisateurs(string fichier)
-        {
-            string jsonListe = JsonConvert.SerializeObject(this.ListeUtilisateur, this.ListeUtilisateur.GetType(), Formatting.Indented, new JsonSerializerSettings
-            {
-                TypeNameHandling = TypeNameHandling.Auto
-            });
-
-            File.WriteAllText(@fichier, jsonListe);
-        }
-
+        
         /// <summary>
         /// Méthode d'entrée dans le programme
         /// </summary>
@@ -87,13 +39,6 @@ namespace ProjetCatalogue
         static void Main(string[] args)
         {
             Application app = new Application();
-            app.SauvegarderUtilisateurs("fichierx.JSON");
-            app.ListeUtilisateur = app.AjouterJSONUtilisateur("fichierx.JSON");
-            Console.WriteLine(app.ListeUtilisateur.GetType());
-            foreach(Utilisateur user in app.ListeUtilisateur)
-            {
-                Console.WriteLine(user);
-            }
             
             Console.WriteLine(new DateOnly());
         }
--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/GestionUtilisateur.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/GestionUtilisateur.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,70 @@ namespace ProjetCatalogue
 {
     public class GestionUtilisateur
     {
+        List<Utilisateur> _listeUtilisateur;
+
+        public List<Utilisateur> ListeUtilisateur { get => _listeUtilisateur; set => _listeUtilisateur = value; }
+
+        public GestionUtilisateur()
+        {
+            ListeUtilisateur = new List<Utilisateur>();
+        }
+
+
+
+        public GestionUtilisateur(List<Utilisateur> listeUtilisateur)
+        {
+            _listeUtilisateur = listeUtilisateur;
+        }
+
+        /// <summary>
+        /// Permet l'ajout d'un utilisateur à la liste d'utilisateurs de l'application
+        /// </summary>
+        /// <param name="user">L'utilisateur à ajouter</param>
+        /// <returns>bool : true si l'ajout a bien été effectué</returns>
+        internal bool AjouterUtilisateur(Utilisateur user)
+        {
+            ListeUtilisateur.Add(user);
+            return ListeUtilisateur.Last() == user;
+        }
+
+        /// <summary>
+        /// Permet de prendre une liste d'utilisateurs et de la sérialiser dans un fichier JSON
+        /// </summary>
+        /// <param name="fichier">Le fichier JSON à utiliser</param>
+        private void SauvegarderUtilisateurs(string fichier)
+        {
+            string jsonListe = JsonConvert.SerializeObject(this.ListeUtilisateur, this.ListeUtilisateur.GetType(), Formatting.Indented, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            });
+
+            File.WriteAllText(@fichier, jsonListe);
+        }
+
+        /// <summary>
+        /// Méthode qui permet la désérialisation d'un fichier JSON pour en extraire des objets C# Utilisateurs
+        /// et les placer dans une liste d'utilisateurs
+        /// </summary>
+        /// <param name="fichier">Le fichier JSON utilisé</param>
+        /// <returns>List(Utilisateur): la liste d'utilisateurs ainsi créée</returns>
+        private List<Utilisateur> AjouterJSONUtilisateur(string fichier)
+        {
+            List<Utilisateur>? listeUtilisateurs = null;
+            try
+            {
+                listeUtilisateurs = JsonConvert.DeserializeObject<List<Utilisateur>>(File.ReadAllText(@fichier), new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Auto
+                });
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Le fichier {0} n'a pas été trouvé", fichier);
+            }
+
+            return listeUtilisateurs;
+
+        }
     }
 }
