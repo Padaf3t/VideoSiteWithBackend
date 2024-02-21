3edeec1f621c3c0a2549c7a8f83adeb16e88f927
veroniquecloutier <2265278@cegeplimoilou.ca>
2024-02-21 10:50:00 -0500

    Gossage de tests TestVideo

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestVideo.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestVideo.cs
@@ -14,14 +14,13 @@ namespace TestProjetCatalogue
     {
 
         [Test]
-        public void etantDonneConstructeurVideo_quandAppelAvecBonnesValeurs_alorsBonnesValeursChamps()
+        public void etantDonneConstructeurVideoAvecTitre_quandAppelAvecBonnesValeurs_alorsBonnesValeursChamps()
         {
             Video videoTest = new Video("Titre bidon");
             Assert.That(videoTest, Is.Not.Null);
             Assert.That(videoTest.Titre, Is.EqualTo("Titre bidon"));
-            Assert.That(videoTest.TypeVideo, Is.EqualTo(Animal.Indetermine));
+            Assert.That(videoTest.TypeVideo, Is.EqualTo(EnumAnimal.Indetermine));
             Assert.That(videoTest.CoteEvaluation, Is.EqualTo(0));
-            Assert.That(videoTest.ListeEvaluations, Is.Not.Null);
             Assert.That(videoTest.DateRealisation, Is.Null);
             Assert.That(videoTest.DureeVideo, Is.EqualTo(0));
             Assert.That(videoTest.Auteur, Is.EqualTo(""));
@@ -30,6 +29,48 @@ namespace TestProjetCatalogue
             Assert.That(videoTest.VideoComplet, Is.EqualTo(""));
             Assert.That(videoTest.Image, Is.EqualTo(""));
         }
+
+        [Test]
+        public void etantDonneConstructeurVideoAvecTitre_quandAppelAvecTitreStringVide_alorsException()
+        {
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Video videoTest = new Video("");
+            });
+
+        }
+
+        [Test]
+        public void etantDonneConstructeurVideoAvecTitre_quandAppelAvecTitreDe4Char_alorsException()
+        {
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Video videoTest = new Video("Test");
+            });
+
+        }
+
+        [Test]
+        public void etantDonneConstructeurTousParam_quandAppelAvecBonnesValeur_alorsBonnesValeursChamps()
+        {
+            Video videoTest = new Video("Test titre", EnumAnimal.Chat, 3.5, null, 5.5, "Bobby", "Mr Miaws", "ressources/extrait/1111.mp4", "ressources/videos/1111.mp4", "ressources/images/1111.mp4");
+            Assert.That(videoTest, Is.Not.Null);
+            Assert.That(videoTest.Titre, Is.EqualTo("Test titre"));
+            Assert.That(videoTest.TypeVideo, Is.EqualTo(EnumAnimal.Chat));
+            Assert.That(videoTest.CoteEvaluation, Is.EqualTo(3.5));
+            Assert.That(videoTest.DateRealisation, Is.Null);
+            Assert.That(videoTest.DureeVideo, Is.EqualTo(5.5));
+            Assert.That(videoTest.Auteur, Is.EqualTo("Bobby"));
+            Assert.That(videoTest.Acteur, Is.EqualTo("Mr Miaws"));
+            Assert.That(videoTest.Extrait, Is.EqualTo("ressources/extrait/1111.mp4"));
+            Assert.That(videoTest.VideoComplet, Is.EqualTo("ressources/videos/1111.mp4"));
+            Assert.That(videoTest.Image, Is.EqualTo("ressources/images/1111.mp4"));
+        }
+
+
+
 
     }
 }
