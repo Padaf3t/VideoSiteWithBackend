05c6082c3fd967a7ed4cf58735fee850c110b690
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-22 08:31:38 -0500

    GestionUtilisateur : Modification nom methode pour Serialisation et Deserialisation Utilisateur

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/GestionUtilisateur.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/Application/GestionUtilisateur.cs
@@ -39,36 +39,36 @@ namespace ProjetCatalogue
         /// <summary>
         /// Permet de prendre une liste d'utilisateurs et de la sérialiser dans un fichier JSON
         /// </summary>
-        /// <param name="fichier">Le fichier JSON à utiliser</param>
-        private void SauvegarderUtilisateurs(string fichier)
+        /// <param name="fichierJSON">Le fichier JSON à utiliser</param>
+        private void SerialisationUtilisateurs(string fichierJSON)
         {
             string jsonListe = JsonConvert.SerializeObject(this.ListeUtilisateur, this.ListeUtilisateur.GetType(), Formatting.Indented, new JsonSerializerSettings
             {
                 TypeNameHandling = TypeNameHandling.Auto
             });
 
-            File.WriteAllText(@fichier, jsonListe);
+            File.WriteAllText(fichierJSON, jsonListe);
         }
 
         /// <summary>
         /// Méthode qui permet la désérialisation d'un fichier JSON pour en extraire des objets C# Utilisateurs
         /// et les placer dans une liste d'utilisateurs
         /// </summary>
-        /// <param name="fichier">Le fichier JSON utilisé</param>
+        /// <param name="fichierJSON">Le fichier JSON utilisé</param>
         /// <returns>List(Utilisateur): la liste d'utilisateurs ainsi créée</returns>
-        private List<Utilisateur> AjouterJSONUtilisateur(string fichier)
+        private List<Utilisateur> DeserialisationJSONUtilisateur(string fichierJSON)
         {
             List<Utilisateur>? listeUtilisateurs = null;
             try
             {
-                listeUtilisateurs = JsonConvert.DeserializeObject<List<Utilisateur>>(File.ReadAllText(@fichier), new JsonSerializerSettings
+                listeUtilisateurs = JsonConvert.DeserializeObject<List<Utilisateur>>(File.ReadAllText(fichierJSON), new JsonSerializerSettings
                 {
                     TypeNameHandling = TypeNameHandling.Auto
                 });
             }
             catch (FileNotFoundException e)
             {
-                Console.WriteLine("Le fichier {0} n'a pas été trouvé", fichier);
+                Console.WriteLine("Le fichier {0} n'a pas été trouvé", fichierJSON);
             }
 
             return listeUtilisateurs;
