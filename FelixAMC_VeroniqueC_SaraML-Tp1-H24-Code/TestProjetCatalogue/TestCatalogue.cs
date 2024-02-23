54678e8617bc92752881726e9eb7450068e214ff
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-02-23 14:57:21 -0500

    TestCatalogue : Ajout SetUp

--- FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestCatalogue.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestCatalogue.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 using ProjetCatalogue;
 
 namespace TestProjetCatalogue
 {
     public class TestCatalogue
     {
-        private Catalogue catalogue = new Catalogue();
-        private Video video = new Video();
+        private Catalogue catalogue;
+        private Video video;
         //TODO: reste a gérer erreur de méthode
+        [SetUp]
+        public void BaseSetUp()
+        {
+            catalogue = new Catalogue();
+            video = new Video(1);
+        }
+
         [Test]
         public void etantDonnerCatalogue_quandAppelConstructeurCatalogue_alorsCatalogueCreer()
         {
-            Assert.That(catalogue.ListeVideos, Is.Not.Null);
+            Catalogue catalogueTest = new Catalogue();
+            Assert.That(catalogueTest.ListeVideos, Is.Not.Null);
         }
+
         [Test]
-        public void etantDonnerCatalogueAvecVideoPresente_quandVideoAjouter_alorsVideoAjouterDansLaListe()
+        public void etantDonnerCatalogueVide_quandVideoAjouter_alorsVideoAjouterDansLaListe()
         {
 
             catalogue.AjouterVideo(video);
