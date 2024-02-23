32a754c1ccb1de3a072419450a1721c0c693913e
Sara-Maud <158103864+Sara-Maud@users.noreply.github.com>
2024-02-23 12:00:37 -0500

    Ajout test ajouterUilisateur

---
+++ FelixAMC_VeroniqueC_SaraML-Tp1-H24-Code/TestProjetCatalogue/TestGestionUtilisateur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+using ProjetCatalogue;
+
+namespace TestProjetCatalogue
+{
+    internal class TestGestionUtilisateur
+    {
+        private Utilisateur utilisateur = new Utilisateur("TestGestionUtil", "abcd1234!");
+        [Test]
+        public void etantAjoutUtilisateurAvecDonnePareil_quandAjouterUtilisateur_alorsErreur()
+        {
+            GestionUtilisateur gestion = new GestionUtilisateur();
+
+            gestion.AjouterUtilisateur(utilisateur);
+
+            var erreur = Assert.Throws<ArgumentException>(
+               delegate { gestion.AjouterUtilisateur(utilisateur); ; });
+
+            Assert.That(erreur.Message, Is.EqualTo("L'utilisateur TestGestionUtil existe déjà"));
+        }
+    }
+}
