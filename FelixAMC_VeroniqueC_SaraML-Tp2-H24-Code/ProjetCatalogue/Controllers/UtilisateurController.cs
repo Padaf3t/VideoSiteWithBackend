ff7dc5d297d750a8a7f5f3d919442fcf3611f87c
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-04-05 09:14:44 -0400

    Ajouter constructeur et appel de model dans le UtilisateurController
    Ajout de l,appel de model pour la view TousLesMedias et MesFavoris

--- FelixAMC_VeroniqueC_SaraML-Tp2-H24-Code/ProjetCatalogue/Controllers/UtilisateurController.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp2-H24-Code/ProjetCatalogue/Controllers/UtilisateurController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetCatalogue.Models;
 
 namespace ProjetCatalogue.Controllers
 {
     public class UtilisateurController : Controller
     {
+        private Catalogue catalogue;
+        private readonly ILogger<UtilisateurController> _logger;
+
+        public UtilisateurController(ILogger<UtilisateurController> logger)
+        {
+            _logger = logger;
+            catalogue = new Catalogue();
+            catalogue.DeserisalisationJSONVideo(PathFinder.PathJsonVideo);
+
+        }
+
         public IActionResult Acceuil(Utilisateur utilisateur)
         {
             ViewBag.Utilisateur = utilisateur;
@@ -11,11 +23,16 @@ namespace ProjetCatalogue.Controllers
         }
         public IActionResult TousLesMedias()
         {
-            return View();
+            return View(catalogue.ListeVideos);
         }
         public IActionResult MesFavoris()
         {
-            return View();
+            GestionFavori gestionFavori = new GestionFavori();
+            gestionFavori.DeserisalisationJSONFavoris(PathFinder.PathJsonFavori);
+
+            List<Video> videoFavorite = catalogue.ObtenirListeVideoFavoris(gestionFavori.ListeFavoris);
+
+            return View(videoFavorite);
         }
 
     }
