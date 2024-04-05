daa01cb380d4961a451d9b78faff425f5885e3a4
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-04-05 08:20:13 -0400

    Ajouter fichier PathFinder avec path fichier JSON

---
+++ FelixAMC_VeroniqueC_SaraML-Tp2-H24-Code/ProjetCatalogue/Models/PathFinder.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetCatalogue.Models
+{
+    public class PathFinder
+    {
+        public const string PathJsonFavori = "Json/favoris.JSON";
+        public const string PathJsonVideo = "Json/videos.JSON";
+        public const string PathJsonEvaluation = "Json/evaluations.JSON";
+        public const string PathJsonUtilisateur = "Json/utilisateurs.JSON";
+
+    }
+}
