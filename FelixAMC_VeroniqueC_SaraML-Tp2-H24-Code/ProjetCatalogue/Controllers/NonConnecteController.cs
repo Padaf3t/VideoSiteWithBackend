f1b149328bb074a268ffd0b2773942de0d4f3c94
veronique cloutier <2265278@cegeplimoilou.ca>
2024-04-13 14:59:44 -0400

    C#doc NonConnecteController

--- FelixAMC_VeroniqueC_SaraML-Tp2-H24-Code/ProjetCatalogue/Controllers/NonConnecteController.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp2-H24-Code/ProjetCatalogue/Controllers/NonConnecteController.cs
@@ -4,11 +4,18 @@ using System.Diagnostics;
 
 namespace ProjetCatalogue.Controllers
 {
+    /// <summary>
+    /// Contrôleur pour ce qui concerne les pages non connecté
+    /// </summary>
     public class NonConnecteController : Controller
     {
         private readonly ILogger<NonConnecteController> _logger;
         GestionUtilisateur gestionUtilisateur;
 
+        /// <summary>
+        /// Constructeur de la classe
+        /// </summary>
+        /// <param name="logger"></param>
         public NonConnecteController(ILogger<NonConnecteController> logger)
         {
             _logger = logger;
@@ -16,12 +23,26 @@ namespace ProjetCatalogue.Controllers
             gestionUtilisateur.DeserialisationJSONUtilisateur(PathFinder.PathJsonUtilisateur);
         }
 
+        /// <summary>
+        /// Action qui déclenche la vue Accueil; vide le TempData
+        /// </summary>
+        /// <returns>la vue Accueil</returns>
         public IActionResult Accueil()
         {
             TempData.Clear();
             return View();
         }
 
+        /// <summary>
+        /// Action qui déclenche la vue Accueil; traite via HTTPPost le contenu des formulaires de connection et d'inscription;
+        /// utilise des méthodes de validation pour en vérifier le contenu; va connecter un utilisateur si formulaire de connection
+        /// (boutonConnection), ou bien créer un utilisateur puis le connecter si formulaire de création d'un compte (boutonInscription).
+        /// Va placer le pseudo utilisateur dans un TempData. Si la connection de l'utilisateur/admin se fait effectivement,
+        /// Va retourner un RedirectAction sur la page d'accueil connectée respective (utilisateur ou admin, selon); sinon, va retourner
+        /// la vue Accueil.
+        /// </summary>
+        /// <param name="idButton"></param>
+        /// <returns>la vue Accueil, ou bien un RedirectAction sur la vue accueil utilisateur ou la vue accueil administrateur</returns>
         [HttpPost]
         public IActionResult ResultatFormulaire(string idButton)
         {
@@ -91,6 +112,10 @@ namespace ProjetCatalogue.Controllers
             return View("Accueil");
         }
 
+        /// <summary>
+        /// Action utilisée si erreur ; retourne une vue d'erreur
+        /// </summary>
+        /// <returns>Une vue d'erreur</returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
