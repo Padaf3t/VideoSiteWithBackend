41545ab1ec168bf9dd9ea643cf940a41f9516968
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-05-03 10:15:46 -0400

    cleanup catalogue et correction bug

--- FelixAMC_VeroniqueC_SaraML-Tp3-H24-Code/ProjetCatalogue/Models/Catalogue.cs
+++ FelixAMC_VeroniqueC_SaraML-Tp3-H24-Code/ProjetCatalogue/Models/Catalogue.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
@@ -44,35 +45,6 @@ namespace ProjetCatalogue.Models
         public DbSet<Video> ListeVideos { get => _listeVideos; set => _listeVideos = value; }
 
         /// <summary>
-        /// Permet l'ajout d'une vidéo à la liste de vidéos du catalogue, après avoir validé que celle-ci n'y est pas déjà
-        /// Une erreur sera lancée et attrapée si la vidéo existe déjà
-        /// </summary>
-        /// <param name="video">La vidéo à ajouter</param>
-        /// <returns>bool : true si l'ajout a bien été effectué; false si la vidéo existait déjà</returns>
-        public bool AjouterVideo(Video video)
-        {
-            bool erreurNote = false;
-
-            try
-            {
-                if (TrouverUneVideo(video.IdVideo) != null)
-                {
-                    throw new ArgumentException("La video " + video.IdVideo + " existe déjà");
-                }
-
-                this.ListeVideos.Add(video);
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-                erreurNote = true;
-            }
-
-            return !erreurNote;
-
-        }
-
-        /// <summary>
         /// Permet de trouver une vidéo selon son id
         /// </summary>
         /// <param name="idVideoAChercher">le id de la vidéo à chercher</param>
@@ -89,27 +61,13 @@ namespace ProjetCatalogue.Models
         /// <returns>Une liste de vidéos</returns>
         public List<Video> ObtenirListeVideoFavorites(List<Favori> listeFavori)
         {
-            IEnumerable < Video > query = 
-                from videoTemp in this.ListeVideos
+            IEnumerable<Video> query =
+                from videoTemp in this.ListeVideos.ToList()
                 join favoriTemp in listeFavori
                 on videoTemp.IdVideo equals favoriTemp.IdVideo
                 select videoTemp;
 
             return query.ToList();
-        }
-        
-        /// <summary>
-        /// Méthode ToString de la classe.
-        /// </summary>
-        /// <returns>string : chaine contenant la liste des vidéos du catalogue</returns>
-        public override string ToString()
-        {
-            string catalogueVideo = "Catalogue de vidéos animales : \n\n";
-            foreach (Video video in this.ListeVideos) 
-            { 
-                catalogueVideo += video.ToString() + "\n";
-            }
-            return catalogueVideo;
         }
 
     }
